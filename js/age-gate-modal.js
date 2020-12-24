5e0ee8b2c839dfddf588563d7a8a03ff2a89d64d
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-12-24 15:12:52 -0500

    infra: add outOfStockModal

--- js/age-gate-modal.js
+++ js/age-gate-modal.js
@@ -19,34 +19,34 @@ window.addEventListener("DOMContentLoaded", event => {
     },
   });
 
-  setAgeGateModalContent();
+  setAgeGateModalContent(ageGateModal);
 
   // open modal
   ageGateModal.open();
-
-  function setAgeGateModalContent() {
-    ageGateModal.setContent(
-      "<h3>Are you above the age of 18?</h3><p>Please tell me the truth</p>"
-    );
-
-    // add a button
-    ageGateModal.addFooterBtn(
-      "Yes, 18 or older",
-      "tingle-btn tingle-btn--primary",
-      function () {
-        // here goes some logic
-        ageGateModal.close();
-      }
-    );
-
-    // add another button
-    ageGateModal.addFooterBtn(
-      "No, Im a minor",
-      "tingle-btn tingle-btn--danger",
-      function () {
-        // drop cookie to prevent reentry for a period of time
-        ageGateModal.close();
-      }
-    );
-  }
 });
+
+function setAgeGateModalContent(modal) {
+	modal.setContent(
+		"<h3>Are you above the age of 18?</h3><p>Please tell me the truth</p>"
+	);
+
+	// add a button
+	modal.addFooterBtn(
+		"Yes, 18 or older",
+		"tingle-btn tingle-btn--primary",
+		function () {
+			// here goes some logic
+			modal.close();
+		}
+	);
+
+	// add another button
+	modal.addFooterBtn(
+		"No, Im a minor",
+		"tingle-btn tingle-btn--danger",
+		function () {
+			// drop cookie to prevent reentry for a period of time
+			modal.close();
+		}
+	);
+}
